8f36663d398f07b47551438cfca6e170dd62d2d1
Florian Uhlig <f.uhlig@gsi.de>
2024-05-06 17:00:27 +0200

    Add test to test some functionality from geobase
        Unfortunately the code is heavily interwoven such that it is not easy to test
        single fucntions or classes. To test the read function of FairGeoSet on needs
        to setup some environment which is needed. One needs a mother volume and also
        media information as well as access to the shape class.
        Currently the geometry for a box is read from file and printed. No real check
        is done.

---
+++ tests/geobase/FairTestGeo.cxx
@@ -0,0 +1,16 @@
+/********************************************************************************
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+#include "FairTestGeo.h"
+
+FairTestGeo::FairTestGeo()
+    : FairGeoSet()
+{
+    // Constructor
+    SetName("testdet");
+    setMaxModules(10);
+}
