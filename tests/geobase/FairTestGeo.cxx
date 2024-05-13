0311adcf187d1c2f864f497b34fdd1141ee73b10
Florian Uhlig <f.uhlig@gsi.de>
2024-05-13 16:42:54 +0200

    Fix license header and code cleanup

--- tests/geobase/FairTestGeo.cxx
+++ tests/geobase/FairTestGeo.cxx
@@ -1,16 +1,13 @@
-/********************************************************************************
- * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
- *                                                                              *
- *              This software is distributed under the terms of the             *
- *              GNU Lesser General Public Licence (LGPL) version 3,             *
- *                  copied verbatim in the file "LICENSE"                       *
- ********************************************************************************/
+/***************************************************************************
+ * Copyright (C) 2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ *                                                                         *
+ *            This software is distributed under the terms of the          *
+ *            GNU Lesser General Public Licence (LGPL) version 3,          *
+ *                copied verbatim in the file "LICENSE"                    *
+ ***************************************************************************/
 #include "FairTestGeo.h"
 
 FairTestGeo::FairTestGeo()
-    : FairGeoSet()
+    : FairGeoSet("testdet",10)
 {
-    // Constructor
-    SetName("testdet");
-    setMaxModules(10);
 }
